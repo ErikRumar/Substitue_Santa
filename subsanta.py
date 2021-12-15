61b1a3d34e59c86364868e0bc56b24f81ee4c843
ErikRumar <96128314+ErikRumar@users.noreply.github.com>
2021-12-15 09:08:18 +0100

    Subsanta part ll

--- subsanta.py
+++ subsanta.py
@@ -1,3 +1,4 @@
+import time
 
 global choice
 
@@ -39,15 +40,45 @@ def read():
     choice = -1
 
 
+def createn():
+    global choice
+    amount = int(input("How many naughty children: "))
+    with open(f"naughtychildrenlist.txt", "w", encoding="utf8") as lista:
+        for x in range(amount):
+            print(x+1, end= ": ")
+            add = input("")
+            if x < amount:
+                lista.write(add)
+                lista.write("\n")
+            else:
+                lista.write(add)
+    choice = -1
+
+
+def readn():
+    global choice
+    with open(f"naughtychildrenlist.txt", "r", encoding= "utf8") as lista:
+        time.sleep(0.5)
+        print("Naughtlist:\n")
+        time.sleep(1)
+        lin = 0
+        for x in lista.readlines():
+            lin += 1
+            print(lin, x)
+    choice = -1
+
+
 
 def menu():
     choice = -1
-    while choice < 1 or choice > 3:
+    while choice < 1 or choice > 5:
         print("""
                 Meny
         
         1. Skapa önskelista
         2. Läs önskelista
+        3. Skapa naughty list
+        4. Läs naughty list
 
 
         """)
@@ -59,7 +90,15 @@ def menu():
             read()
             choice = -1
         if choice == 3:
+            createn()
+            choice = -1
+        if choice == 4:
+            readn()
+            choice = -1
+        if choice == 3:
             break
         else:
+            time.sleep(0.5)
             print("returned")
+            time.sleep(1)
 menu()
